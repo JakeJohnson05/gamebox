e897d40475b123de57372f0b9f01f25efd93742c
Jake Johnson <jakejohnson801@outlook.com>
2019-04-02 23:48:22 -0600

    HomePage set up

---
+++ index.js
@@ -0,0 +1,13 @@
+const EXPRESS = require('express');
+const APP = EXPRESS();
+const PORT = process.env.PORT || 5000;
+
+
+APP.use(EXPRESS.static('gamebox/static'));
+APP.use(EXPRESS.static('favicon'));
+
+
+APP.get('/', (req, res) => res.sendFile('home.html', {root: `${ __dirname }/gamebox`}));
+
+
+APP.listen(PORT, () => console.log(`gamebox active on port: ${ PORT }`));
